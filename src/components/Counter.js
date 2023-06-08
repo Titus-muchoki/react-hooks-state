3c877b652bef5e53ed971a654224b25079c4f7f2
kajela <titoyut56@gmail.com>
2023-06-08 04:53:37 -0400

    add the initial files

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,12 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    // setCount(count + 2);
+    // setCount(count + 1);
+    setCount((currentCounr) => count +1);
+    setCount((currentCounr) => count +1);
+
+
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
